1df2cf63a1620f450698044d7bcdf577bea15f30
Amit Metkar <amit.metkat@gmail.com>
2017-08-26 23:09:42 +0530

    child routing and auth guard

---
+++ src/app/auth-guard.service.ts
@@ -0,0 +1,30 @@
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router
+} from '@angular/router';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { AuthService } from './auth.service';
+
+@Injectable()
+
+export class AuthGaurdService implements CanActivate {
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(route: ActivatedRouteSnapshot,
+            state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
+    return this.authService.isAuthenticated().then(
+      (authenticated: boolean) => {
+        if (authenticated) {
+          return true;
+        } else {
+          this.router.navigate(['/']);
+        }
+      }
+    );
+  }
+}
--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { ServersService } from '../servers.service';
 
@@ -12,18 +12,32 @@ export class EditServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
-
+  allowEdit = false;
   constructor(private serversService: ServersService, private route: ActivatedRoute) { }
 
   ngOnInit() {
 
-    console.log(this.route.snapshot.queryParams);
-    console.log(this.route.snapshot.fragment);
+    // console.log(this.route.snapshot.queryParams);
+    // console.log(this.route.snapshot.fragment);
 
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe(
+      (queryParams: Params) => { this.allowEdit = queryParams['allowEdit'] === '1' ? true : false; }
+    );
+    this.allowEdit = this.route.snapshot.queryParams['allowEdit'] === '1' ? true : false;
     this.route.fragment.subscribe();
 
-    this.server = this.serversService.getServer(1);
+    this.route.params.subscribe(
+      (params: Params) => {
+        const id = +params['id'];
+        this.updateServer(id);
+      }
+    );
+    const id = +this.route.snapshot.params['id'];
+    this.updateServer(id);
+  }
+
+  private updateServer(id) {
+    this.server = this.serversService.getServer(id);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
